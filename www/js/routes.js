7cec97027dd56298128161d4e6cad55990ebf0c1
Yojana Rangnekar <yojana.rangnekar92@gmail.com>
2017-01-04 21:50:44 +0530

    Initial import

---
+++ www/js/routes.js
@@ -0,0 +1,43 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.visionSndMission', {
+    url: '/visionMission',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/visionSndMission.html',
+        controller: 'visionSndMissionCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/home')
+
+  
+
+});
